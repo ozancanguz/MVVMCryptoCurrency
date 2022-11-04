e69db3feacd5e21df83f3e43c711041501775bc7
Ozan <ozancangu24@gmail.com>
2022-11-04 14:50:17 +0300

    - CoinViewModel.kt coded

---
+++ app/src/main/java/com/ozancanguz/mvvmcryptocurrency/viewmodel/CoinViewModel.kt
@@ -0,0 +1,48 @@
+package com.ozancanguz.mvvmcryptocurrency.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ozancanguz.mvvmcryptocurrency.Service.CoinService
+import com.ozancanguz.mvvmcryptocurrency.model.Coin
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+
+class CoinViewModel:ViewModel() {
+
+
+    private val disposable= CompositeDisposable()
+    private val coinservice=CoinService()
+
+    private var coins=MutableLiveData<List<Coin>>()
+
+    fun refresh(){
+        fetchFromRemote()
+    }
+
+
+   fun fetchFromRemote(){
+           disposable.add(
+               coinservice.getData()
+                   .subscribeOn(Schedulers.newThread())
+                   .observeOn(AndroidSchedulers.mainThread())
+                   .subscribeWith(object :DisposableSingleObserver<List<Coin>>(){
+                       override fun onSuccess(t: List<Coin>?) {
+                        coins.value=t!!
+
+                       }
+
+                       override fun onError(e: Throwable?) {
+                          Log.d("hata","veri cekilemedi")
+                       }
+
+
+                   })
+
+           )
+
+   }
+
+}
